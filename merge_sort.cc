c9ac36282c6ee5d44f18a14cb18ef00d74237f08
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-09-22 01:36:33 +0530

    merge_sort

---
+++ merge_sort.cc
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+         
+         int n1 = m - l + 1;
+         int n2 = r - m;
+         
+         int L[n1] , R[n2];
+         
+         // fill l
+         int k = l;
+         for(int i=0;i<n1;++i){
+             L[i] = arr[k++];
+         }
+         
+         for(int i=0;i<n2;++i){
+             R[i] = arr[k];
+             k++;
+         }
+         
+         // merge them in arr
+         int i = 0;
+         int j = 0;
+         k = l;
+         
+         while(i<n1 && j<n2){
+             if(L[i] <= R[j]){
+                 arr[k] = L[i];
+                 i++;
+             }
+             else{
+                 arr[k] = R[j];
+                 j++;
+             }
+             k++;
+         }
+         
+         while(i<n1){
+             arr[k] = L[i];
+             i++;
+             k++;
+         }
+         
+         while(j<n2){
+             arr[k] = R[j];
+             j++;
+             k++;
+         }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l>=r){
+            return;
+        }
+        
+        int mid = l + (r-l)/2;
+        
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        
+        merge(arr,l,mid,r);
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
