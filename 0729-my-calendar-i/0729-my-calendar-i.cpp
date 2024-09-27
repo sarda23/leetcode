af33645998b839b5edbfd563309e181133def1cb
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-09-27 23:42:30 +0530

    Time: 74 ms (74.23%), Space: 41.7 MB (86.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,34 +1,21 @@
-// for optimization
-// ordered set - insertion - O(logn)
-    // search - Binary search use hota hai isme - O(logn) 
-// or we can use ordered map
 class MyCalendar {
 public:
-
-    set<pair<int,int>> st;  // space - number of event
+    vector<pair<int,int>> bookings;
     MyCalendar() {
         
     }
     
+      bool check_overlap(int start1,int end1,int start2,int end2){
+        return max(start1,start2) < min(end1,end2);
+    }
+
     bool book(int start, int end) {
-        //find the first event that starts after or at the same time of (start,end)
-        //>=  - lower bound ki baat ho rahi hai
-        auto it = st.lower_bound({start,end});//log(n)
-
-        //check if the current event overlaps with the next event
-        if(it != st.end() && it->first < end){
-            return false;
-        }
-
-        // check if the current event overlaps with the previous event
-        if(it != st.begin()){
-            auto prevIt = prev(it);
-            if(start < prevIt->second){
+        for(pair<int,int> booking : bookings){
+            if(check_overlap(booking.first,booking.second,start,end)){
                 return false;
             }
         }
-
-        st.insert({start,end});
+        bookings.push_back({start,end});
         return true;
     }
 };
