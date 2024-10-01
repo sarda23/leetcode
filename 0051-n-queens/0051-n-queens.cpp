3ec87e46916c3c5ffdb0b10970b8adb60502bf3a
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-02 02:35:42 +0530

    Time: 9 ms (29.86%), Space: 12.7 MB (25.36%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -3,30 +3,33 @@ public:
 
     vector<vector<string>> result;
     int N;
+    
+    unordered_set<int> cols;
+    unordered_set<int> diag;
+    unordered_set<int> anti_diag;
+    // bool isValid(vector<string> &board, int row, int col){
+    //     // look upward
+    //     for(int i = row-1;i>=0;--i){
+    //         if(board[i][col] == 'Q'){
+    //             return false;
+    //         }
+    //     }
 
-    bool isValid(vector<string> &board, int row, int col){
-        // look upward
-        for(int i = row-1;i>=0;--i){
-            if(board[i][col] == 'Q'){
-                return false;
-            }
-        }
+    //     // check left diagonal upwards
+    //     for(int i = row-1, j=col-1;i>=0 && j>=0;--i,--j){
+    //         if(board[i][j] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     // check right diagonal upwards
+    //     for(int i = row-1, j=col+1;i>=0 && j<N;--i,++j){
+    //         if(board[i][j] == 'Q'){
+    //             return false;
+    //         }
+    //     }
 
-        // check left diagonal upwards
-        for(int i = row-1, j=col-1;i>=0 && j>=0;--i,--j){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        // check right diagonal upwards
-        for(int i = row-1, j=col+1;i>=0 && j<N;--i,++j){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-
-        return true;
-    }
+    //     return true;
+    // }
 
     void solve(vector<string> &board,  int row){
         if(row>=N){
@@ -35,21 +38,44 @@ public:
         }
 
         for(int col=0;col<N;++col){
-            if(isValid(board,row,col)){
-                board[row][col] = 'Q';
+            // if(isValid(board,row,col)){
+            //     board[row][col] = 'Q';
 
-                solve(board,row+1);
+            //     solve(board,row+1);
 
-                board[row][col] = '.';
+            //     board[row][col] = '.';
+            // }
+
+            int diag_constant = row + col;
+            int anti_diag_const = row - col;
+
+            if(cols.find(col) != cols.end() || 
+            diag.find(diag_constant) != diag.end() ||
+            anti_diag.find(anti_diag_const) != anti_diag.end()){
+                continue;
             }
-        }
+            // for a given [row][col] = 'Q
+            cols.insert(col);
+            diag.insert(diag_constant);
+            anti_diag.insert(anti_diag_const);
+
+            board[row][col] = 'Q';
+            solve(board,row+1);
+
+            cols.erase(col);
+            diag.erase(diag_constant);
+            anti_diag.erase(anti_diag_const);
+            board[row][col] = '.';
+        }   
     }
 
     vector<vector<string>> solveNQueens(int n) {
         
         vector<string> board(n, string(n,'.')); // n=3 ->> {"...","...","..."}
         N = n;
-        solve(board,0);
+        
+        
+        solve(board,0); // starting from row 0
 
         return result;
     }
