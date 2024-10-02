1714ae46dd742add27382a0a41ccb8f643cc9cb3
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-02 22:52:33 +0530

    Time: 3 ms (60.66%), Space: 12.5 MB (77.1%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,43 +3,46 @@ public:
 
     vector<string> result;
 
-    bool isValid(string &str){
-        int count = 0;
-        for(char &ch:str){
-            if(ch == '('){
-                count++;
-            }
-            else{
-                count--;
-            }
-            if(count<0){
-                return false;
-            }
-        }
-        return count==0;
-    }
+    // bool isValid(string &str){
+    //     int count = 0;
+    //     for(char &ch:str){
+    //         if(ch == '('){
+    //             count++;
+    //         }
+    //         else{
+    //             count--;
+    //         }
+    //         if(count<0){
+    //             return false;
+    //         }
+    //     }
+    //     return count==0;
+    // }
 
-    void solve(string &curr, int n){
+    void solve(string &curr, int n,int open,int close){
         if(curr.length() == 2*n){
-            if(isValid(curr)){
-                result.push_back(curr);
-            }
+            result.push_back(curr);
             return;
         }
 
+        if(open<n){
         curr.push_back('(');
-        solve(curr,n);
+        solve(curr,n,open+1,close);
         curr.pop_back();
-
+        }
+        if(close<open){
         curr.push_back(')');
-        solve(curr,n);
+        solve(curr,n,open,close+1);
         curr.pop_back();
+        }
     }
     
     vector<string> generateParenthesis(int n) {
         string curr ="";
-
-        solve(curr,n);
+        
+        int open = 0;
+        int close = 0;
+        solve(curr,n,open,close);
 
         return result;
     }
