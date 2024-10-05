acc9cec0385cfc89acc488ddb4ee3e9c523a7bfa
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-05 17:38:02 +0530

    Time: 4 ms (32.85%), Space: 9.5 MB (90.67%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int t[101];
+
+    int solve(vector<int> &nums,int i,int n){
+        if(i>n){
+            return 0;
+        }
+        if(t[i] != -1){
+            return t[i];
+        }
+
+        int take = nums[i] + solve(nums,i+2,n);
+        int skip = solve(nums,i+1,n);
+
+        return t[i] = max(take , skip);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1){
+            return nums[0];
+        }
+
+        if(n == 2){
+            return max(nums[0] , nums[1]);
+        }
+
+        memset(t,-1,sizeof(t));
+
+        // case 1 -- take 1st house - 0'th index wala house
+
+        int take_0th = solve(nums,0,n-2);
+
+        memset(t,-1,sizeof(t));
+        // case 2 -- take the 2nd house 
+        int take_1st = solve(nums,1,n-1);
+
+        return max(take_0th,take_1st);
+    }
+};
