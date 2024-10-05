b9f73517d91f5f442842e5c1133d9c1d36e4df9f
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-05 18:03:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (36.16%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int t[101];
+    // int t[101];
 
-    int solve(vector<int> &nums,int i,int n){
-        if(i>n){
-            return 0;
-        }
-        if(t[i] != -1){
-            return t[i];
-        }
+    // int solve(vector<int> &nums,int i,int n){
+    //     if(i>n){
+    //         return 0;
+    //     }
+    //     if(t[i] != -1){
+    //         return t[i];
+    //     }
 
-        int take = nums[i] + solve(nums,i+2,n);
-        int skip = solve(nums,i+1,n);
+    //     int take = nums[i] + solve(nums,i+2,n);
+    //     int skip = solve(nums,i+1,n);
 
-        return t[i] = max(take , skip);
-    }
+    //     return t[i] = max(take , skip);
+    // }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -26,17 +26,50 @@ public:
         if(n == 2){
             return max(nums[0] , nums[1]);
         }
+        // 2nd approach
+        vector<int> t(n+1,0);
+        //t[i] -- money stolen when you have i houses
 
-        memset(t,-1,sizeof(t));
+        t[0] = 0;
 
-        // case 1 -- take 1st house - 0'th index wala house
+        //case 1  take 1st house hence skip the last house
 
-        int take_0th = solve(nums,0,n-2);
+        for(int i = 1;i<=n-1;++i){
+            int skip = t[i-1];
 
-        memset(t,-1,sizeof(t));
-        // case 2 -- take the 2nd house 
-        int take_1st = solve(nums,1,n-1);
+            int take = nums[i-1] + ((i-2<0) ? 0 : t[i-2]);
 
-        return max(take_0th,take_1st);
+            t[i] = max(skip,take);
+        }
+        int result_1 = t[n-1];
+
+        t.clear();
+        // case 2 - skip 1st house - u can take the last house
+        t[0] = 0;
+        t[1] = 0; // skip 1st house
+        for(int i = 2;i<=n;++i){
+            int skip = t[i-1];
+
+            int take = nums[i-1] + ((i-2<0) ? 0 : t[i-2]);
+            
+            t[i] = max(skip,take);
+        }
+        int result_2 = t[n];
+
+        return max(result_1,result_2);
+
+        // 1st approach
+        // memset(t,-1,sizeof(t));
+
+        // // case 1 -- take 1st house - 0'th index wala house
+
+        // int take_0th = solve(nums,0,n-2);
+
+        // memset(t,-1,sizeof(t));
+
+        // // case 2 -- take the 2nd house - 1st index wala house
+        // int take_1st = solve(nums,1,n-1);
+
+        // return max(take_0th,take_1st);
     }
 };
