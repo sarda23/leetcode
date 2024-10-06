bd18f33844da40de16405b03b230f74f8ebaf062
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-06 13:06:44 +0530

    Time: 4 ms (40.09%), Space: 8.6 MB (35.48%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -5,31 +5,55 @@ public:
             swap(s1,s2); // to have s1 as the longest string all the time
         }
 
-        vector<string> vec1, vec2;
+        deque<string> d1, d2;
         stringstream ss1(s1);
         string token;
         while(ss1 >> token){
-            vec1.push_back(token);
+            d1.push_back(token);
         }
 
         stringstream ss2(s2);
         while(ss2 >> token){
-            vec2.push_back(token);
+            d2.push_back(token);
         }
 
-        int i = 0, j = vec1.size()-1; // s1 vec1
-        int k = 0, l = vec2.size()-1; // s2 vec2
-
-        while(k<vec2.size() && i<vec1.size() && vec2[k] == vec1[i]){
-            k++;
-            i++;
+        while(!d1.empty() && !d2.empty() && d1.front() == d2.front()){
+            d1.pop_front();
+            d2.pop_front();
         }
-
-        while(l>=k && vec2[l] == vec1[j]){
-            j--;
-            l--;
+        
+        while(!d1.empty() && !d2.empty() && d1.back() == d2.back()){
+            d1.pop_back();
+            d2.pop_back();
         }
+        return d2.empty();
 
-        return l<k;
+        // 1st method 
+        // vector<string> vec1, vec2;
+        // stringstream ss1(s1);
+        // string token;
+        // while(ss1 >> token){
+        //     vec1.push_back(token);
+        // }
+
+        // stringstream ss2(s2);
+        // while(ss2 >> token){
+        //     vec2.push_back(token);
+        // }
+
+        // int i = 0, j = vec1.size()-1; // s1 vec1
+        // int k = 0, l = vec2.size()-1; // s2 vec2
+
+        // while(k<vec2.size() && i<vec1.size() && vec2[k] == vec1[i]){
+        //     k++;
+        //     i++;
+        // }
+
+        // while(l>=k && vec2[l] == vec1[j]){
+        //     j--;
+        //     l--;
+        // }
+
+        // return l<k;
     }
 };
