874992ebe72510cb37c5bbe76c5767422c58bb07
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-16 00:02:35 +0530

    Time: 198 ms (47.98%), Space: 75.8 MB (94.44%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
-         long long sum = 0;
+        long long sum = 0;
 
-        priority_queue<int> pq(begin(nums), end(nums)); //First time creation of heap "heapify" is O(n)
-        //Refer - https://stackoverflow.com/questions/9755721/how-can-building-a-heap-be-on-time-complexity
-        //Or you can refer NPTEL video on Heap on youtube. 
+        priority_queue<int> pq(begin(nums),end(nums)); // Heapify O(n)
 
-        while(k--) {
-            int maxEl = pq.top();
+        while(k--){
+            int maxE = pq.top();
             pq.pop();
 
-            sum += maxEl;
-            maxEl = ceil(maxEl/3.0);
-            pq.push(maxEl);
+            sum += maxE;
+
+            maxE = ceil(maxE/3.0);
+
+            pq.push(maxE);
         }
 
         return sum;
+
     }
 };
