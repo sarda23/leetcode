777a3fa70e58491ea5c6d0e564ab158cb3b80ea2
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-28 19:18:34 +0530

    Time: 48 ms (32.82%), Space: 176.9 MB (14%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,51 @@
+class MyHashSet {
+public:
+    int M; // number of buckets
+    vector<list<int>> buckets; // separate chaining
+
+    // hash function
+    int getIndex(int key){
+        return key%M;
+    }
+
+    MyHashSet() {
+         M = 150000; // by seeing constraint
+         buckets = vector<list<int>> (M,list<int>{});
+    }
+    
+    void add(int key) {
+        int index = getIndex(key);
+        // kis list me search karenge buckets[index]  me 
+        auto itr = find(buckets[index].begin(), buckets[index].end(), key);
+
+        if(itr == buckets[index].end()){ 
+            buckets[index].push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        int index = getIndex(key);
+
+        auto itr = find(buckets[index].begin(), buckets[index].end(), key);
+
+        if(itr != buckets[index].end()){
+            buckets[index].erase(itr);
+        }
+    }
+    
+    bool contains(int key) {
+        int index = getIndex(key);
+
+        auto itr = find(buckets[index].begin(), buckets[index].end(), key);
+
+        return itr != buckets[index].end();
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
