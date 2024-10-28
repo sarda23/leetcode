77555ccf6acb6ade32416e2e96c5e1a006f8ad20
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-10-28 12:48:11 +0530

    Time: 82 ms (71.11%), Space: 109 MB (63.34%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-       unordered_map<int,int> mp;
+        // without sorting
+        unordered_set<int> st(begin(nums),end(nums));
+        int n = nums.size();
+        int maxStreak = 0;
 
-       sort(begin(nums),end(nums));
+        for(int i=0;i<n;++i){
+            int streak = 0;
+            long long curr = nums[i];
 
-       int maxStreak = 0;
-
-       for(int &num:nums){
-        // check if this number is a perfect square
-        int root = (int)sqrt(num);
-
-        if(root*root == num && mp.find(root) != mp.end()){
-            mp[num] = mp[root] + 1;
-        }else{
-            mp[num] = 1;
+            while(st.find(curr) != st.end()){
+                streak++;
+                if(curr*curr >1e5){ // this can be interesting in interview discussion - if they give you the constraint - this while loop will only run for at most 5 time (2 4 16 256 65536 .... ) so overall the T.C - O(n) 
+                    break;
+                }
+                curr = curr*curr;
+            }
+            maxStreak = max(maxStreak,streak);
         }
-        maxStreak = max(maxStreak,mp[num]);
-       } 
+        return maxStreak<2?-1:maxStreak;
 
-       return maxStreak<2 ? -1 : maxStreak;
+        // with sorting 
+    //    unordered_map<int,int> mp;
+
+    //    sort(begin(nums),end(nums));
+
+    //    int maxStreak = 0;
+
+    //    for(int &num:nums){
+    //     // check if this number is a perfect square
+    //     int root = (int)sqrt(num);
+
+    //     if(root*root == num && mp.find(root) != mp.end()){
+    //         mp[num] = mp[root] + 1;
+    //     }else{
+    //         mp[num] = 1;
+    //     }
+    //     maxStreak = max(maxStreak,mp[num]);
+    //    } 
+
+    //    return maxStreak<2 ? -1 : maxStreak;
     }
 };
