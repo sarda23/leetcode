75c2a44147733f08f4b2a48146c6dee89ef8cd2b
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-11-02 23:13:07 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.91%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.length();
+
+        for (int i = 0; i < n; i++) {
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1])
+                return false;
+        }
+
+        return sentence[0] == sentence[n - 1]; //check last and first
+    }
+};
