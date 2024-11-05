63d6948ffca347a2e0ad9b5bf8a49cdac4a0170d
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-11-05 19:44:49 +0530

    Time: 0 ms (100%), Space: 12.1 MB (87.26%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
     int minChanges(string s) {
-       int n = s.length();
+        int n = s.length();
+        int changes = 0;
 
-       char ch = s[0];
-       int count = 0;
-
-       int changes = 0; // ans
-
-       for(int i=0;i<n;i++){
-        if(s[i] == ch){
-            count++;
-            continue;
+        for(int i=0;i<n;i+=2){
+            if(s[i] != s[i+1]){
+                changes++;
+            }
         }
+        return changes;
 
-        // current character s[i] is not equal to ch (prev character)
-        if(count%2 == 0){
-            // previous substring is even
-            count = 1;// start new substring
-        }
-        else{
-            // s[i] ko change karna padega to make even substring length
-            changes += 1;
-            count = 0;
-        }
-        ch = s[i]; // update to new character
-       } 
+    //    int n = s.length();
 
-       return changes;
+    //    char ch = s[0];
+    //    int count = 0;
+
+    //    int changes = 0; // ans
+
+    //    for(int i=0;i<n;i++){
+    //     if(s[i] == ch){
+    //         count++;
+    //         continue;
+    //     }
+
+    //     // current character s[i] is not equal to ch (prev character)
+    //     if(count%2 == 0){
+    //         // previous substring is even
+    //         count = 1;// start new substring
+    //     }
+    //     else{
+    //         // s[i] ko change karna padega to make even substring length
+    //         changes += 1;
+    //         count = 0;
+    //     }
+    //     ch = s[i]; // update to new character
+    //    } 
+
+    //    return changes;
     }
 };
