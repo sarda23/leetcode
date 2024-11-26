be60a6e33172a126bcf25b49e8d3f0838100e436
Nishant Sarda <60655405+sarda23@users.noreply.github.com>
2024-11-26 23:12:25 +0530

    Time: 2 ms (95.91%), Space: 10.7 MB (97.5%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.cpp
+++ 0773-sliding-puzzle/0773-sliding-puzzle.cpp
@@ -2,57 +2,54 @@ class Solution {
 public:
     int slidingPuzzle(vector<vector<int>>& board) {
         string start = "";
-        for(int i = 0; i < 2; i++) {
-            for(int j = 0; j < 3; j++) {
+        for(int i = 0;i<2;i++){
+            for(int j=0;j<3;j++){
                 start += to_string(board[i][j]);
             }
         }
-        //positions = 6
-        //Total possible states = 6!
-        //Worst case me visit all states = O(6!)
-        //Space : O(6!)
-        string target = "123450";
 
-        queue<string> que;
-        que.push(start);
+        string target = "123450"; 
+
+        queue<string> q;
+        q.push(start);
+
         unordered_map<int, vector<int>> mp;
-        mp[0] = {1, 3};
-        mp[1] = {0, 2, 4};
-        mp[2] = {1, 5};
-        mp[3] = {0, 4};
-        mp[4] = {1, 3, 5};
-        mp[5] = {2, 4};
+        // 0 kon kon si position se swap ho sakhta hai
+        mp[0] = {1,3};
+        mp[1] = {0,2,4};
+        mp[2] = {1,5};
+        mp[3] = {0,4};
+        mp[4] = {1,3,5};
+        mp[5] = {2,4};
 
         unordered_set<string> visited;
         visited.insert(start);
 
-        int level = 0; //moves
-        while(!que.empty()) {
-            int n = que.size();
+        int level = 0; // shortest steps needed 
 
-            //Processing all elements at current level
-            while(n--) {
-                string curr = que.front();
-                que.pop();
+        while(!q.empty()){
+            int n = q.size();
+            // iss level me jitne element hai sabko process karna hai
+            while(n--){
+                string curr = q.front();
+                q.pop();
 
-                if(curr == target) {
+                if(curr == target){
                     return level;
                 }
 
-                int indexOfZero = curr.find('0'); //finding index of 0
-                for(int swapIdx : mp[indexOfZero]) {
-                    string newState = curr;
-                    swap(newState[indexOfZero], newState[swapIdx]);
-                    if(visited.find(newState) == visited.end()) {
-                        que.push(newState);
-                        visited.insert(newState);
+                int index_of_zero = curr.find('0'); // finding index of 0
+                for(int swap_idx :mp[index_of_zero]){
+                    string new_state = curr;
+                    swap(new_state[index_of_zero], new_state[swap_idx]);
+                    if(visited.find(new_state) == visited.end()){
+                        visited.insert(new_state);
+                        q.push(new_state);
                     }
                 }
             }
             level++;
         }
-
         return -1;
-
     }
 };
